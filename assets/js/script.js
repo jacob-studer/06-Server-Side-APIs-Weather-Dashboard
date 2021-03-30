d64aaa15c796e4091d94b00726ed98f8251b9d28
Jacob Studer <jacob.studer.dev@gmail.com>
2021-03-30 16:42:38 -0600

    js portion done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,30 @@ var searchInputEl = document.querySelector('.search-input');
 var apiKey = '932f9b8978de351c303b867edf8f5b07'
 
 // getting previous result
-JSON.parse(localStorage.getItem('weather'));
-JSON.parse(localStorage.getItem('forecast'));
+// var previousWeather = JSON.parse(localStorage.getItem('weather'));
+// var previousForecast = JSON.parse(localStorage.getItem('forecast'));
 
-//search history - query selector for button, text content = ?
-// localStorage.getItem('weather', WeatherRes);
-// localStorage.getItem('forecast', WeatherRes);
+var searchHistoryArr = JSON.parse(localStorage.getItem('previousSearch')) || [];
+
+function generateButtons(){
+    var btnGroup = document.querySelector('.btn-group-vertical');
+    searchHistoryArr.reverse().forEach(function(city){
+        var cityBtn = document.createElement('button');
+        cityBtn.textContent = city;
+        cityBtn.classList.add('btn');
+        cityBtn.addEventListener('click', handleSearchCitySubmit);
+        btnGroup.append(cityBtn);
+    })
+}
+
+function handleSearchCitySubmit(event) {
+    event.preventDefault();
+    console.log('works');
+
+    var searchInputVal = this.textContent;
+
+    getParams(searchInputVal)
+}
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
@@ -18,25 +36,28 @@ function handleSearchFormSubmit(event) {
     var searchInputVal = document.querySelector('.search-input').value;
 
     if (!searchInputVal) {
-        window.alert('Please enter a City to search for.')
-    }
+        return window.alert('Please enter a City to search for.')
+    } 
 
-    getParams()
+    getParams(searchInputVal)
 }
 
 
 function getParams(query){
-    // var searchParamsArr = document.location.search.split('&');
-    var query = searchInputEl.value;
     console.log(query);
+
+    if (searchHistoryArr.length <= 4) {
+        searchHistoryArr.push(query);
+        localStorage.setItem('previousSearch', JSON.stringify(searchHistoryArr));
+    }
     searchForecast(query); 
     searchCurrentWeather(query);
 }
 
 function searchCurrentWeather(query) {
-    var weatherQueryUrl = 'https://api.openweathermap.org/data/2.5/weather' //?q={city name}&appid={API key}
+    var weatherQueryUrl = 'https://api.openweathermap.org/data/2.5/weather' 
 
-    weatherQueryUrl = weatherQueryUrl + '?&q=' + query + '&appid=' + apiKey;
+    weatherQueryUrl = weatherQueryUrl + '?&q=' + query + '&units=imperial' + '&appid=' + apiKey;
 
     fetch(weatherQueryUrl)
     .then(function (response) {
@@ -56,9 +77,9 @@ function searchCurrentWeather(query) {
 
 
 function searchForecast(query) {
-    var weatherQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast' //?q={city name}&appid={API key}
+    var weatherQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast' 
 
-    weatherQueryUrl = weatherQueryUrl + '?&q=' + query + '&appid=' + apiKey;
+    weatherQueryUrl = weatherQueryUrl + '?&q=' + query + '&units=imperial' + '&appid=' + apiKey;
 
     fetch(weatherQueryUrl)
     .then(function (response) {
@@ -79,6 +100,7 @@ function searchForecast(query) {
 
 
 function printCurrentWeather(weatherRes) {
+    console.log(weatherRes);
     var cityTitleEl = document.querySelector('.city-title');
     var dateTitleEl = document.querySelector('.date-title');
     var weatherIconEl = document.querySelector('.weather-icon');
@@ -87,17 +109,14 @@ function printCurrentWeather(weatherRes) {
     var weatherWindEl = document.querySelector('.weather-wind');
     var weatherUVEl = document.querySelector('.weather-uv');
 
-    console.log(weatherRes);
-
-    localStorage.setItem('weather', JSON.stringify(weatherRes)); //json parse for the weatherRes?
-
-    cityTitleEl.append(weatherRes.name);
-    dateTitleEl.append(weatherRes.dt);
-    weatherIconEl.append(weatherRes.weather.icon);
-    weatherTempEl.append(weatherRes.main.temp);
-    weatherHumidityEl.append(weatherRes.main.humidity);
-    weatherWindEl.append(weatherRes.wind.speed);
-    weatherUVEl.append(weatherRes.main.temp);
+    cityTitleEl.textContent = weatherRes.name;
+    dateTitleEl.textContent = moment.unix(weatherRes.dt).format('l');
+    console.log (dateTitleEl);
+    weatherIconEl.textContent = weatherRes.weather.icon;
+    weatherTempEl.textContent = 'Temperature: ' + weatherRes.main.temp + ' F°';
+    weatherHumidityEl.textContent = 'Humidity: ' + weatherRes.main.humidity + ' %';
+    weatherWindEl.textContent = 'wind speed: ' + weatherRes.wind.speed + ' MPH';
+    weatherUVEl.textContent = 'UV Index: ' + weatherRes.main.temp;
 }
 
 function printForecast(forecastRes) {
@@ -121,37 +140,33 @@ function printForecast(forecastRes) {
     var forecastIcon5 = document.querySelector('.forecast-icon-5');
     var forecastTemp5 = document.querySelector('.forecast-temp-5');
     var forecastHumidity5 = document.querySelector('.forecast-humidity-5');
-    
+ 
 
     console.log(forecastRes);
 
-    localStorage.setItem('forecast', JSON.stringify(forecastRes)); //json parse for the weatherRes?
-
-    forecastDate1.append();
-    forecastIcon1.append( );
-    forecastTemp1.append();
-    forecastDate1.append();
-    forecastHumidity1.append();
-    forecastDate2.append();
-    forecastIcon2.append();
-    forecastTemp2.append();
-    forecastHumidity2.append();
-    forecastDate3.append();
-    forecastIcon3.append();
-    forecastTemp3.append();
-    forecastHumidity3.append();
-    forecastDate4.append();
-    forecastIcon4.append();
-    forecastTemp4.append();
-    forecastHumidity4.append();
-    forecastDate5.append();
-    forecastIcon5.append();
-    forecastTemp5.append();
-    forecastHumidity5.append();
+    forecastDate1.textContent = moment.unix(forecastRes.list[0].dt).format('l');
+    forecastIcon1.innerHTML = `<img src='https://openweathermap.org/img/w/${forecastRes.list[0].weather[0].icon}.png'>`;
+    forecastTemp1.textContent = 'Temp: ' + forecastRes.list[0].main.temp + ' F°';
+    forecastHumidity1.textContent = 'Humidity: ' + forecastRes.list[0].main.humidity + ' %';
+    forecastDate2.textContent = moment.unix(forecastRes.list[1].dt).format('l');
+    forecastIcon2.textContent = forecastRes.list[1].weather[0].icon;
+    forecastTemp2.textContent = 'Temp: ' + forecastRes.list[1].main.temp + ' F°';
+    forecastHumidity2.textContent = 'Humidity: ' + forecastRes.list[1].main.humidity + ' %';
+    forecastDate3.textContent = moment.unix(forecastRes.list[2].dt).format('l');
+    forecastIcon3.textContent = forecastRes.list[2].weather[0].icon;
+    forecastTemp3.textContent = 'Temp: ' + forecastRes.list[2].main.temp + ' F°';
+    forecastHumidity3.textContent = 'Humidity: ' + forecastRes.list[2].main.humidity + ' %';
+    forecastDate4.textContent = moment.unix(forecastRes.list[3].dt).format('l');
+    forecastIcon4.textContent = forecastRes.list[3].weather[0].icon;
+    forecastTemp4.textContent = 'Temp: ' + forecastRes.list[3].main.temp + ' F°';
+    forecastHumidity4.textContent = 'Humidity: ' + forecastRes.list[3].main.humidity + ' %';
+    forecastDate5.textContent = moment.unix(forecastRes.list[4].dt).format('l');
+    forecastIcon5.textContent = forecastRes.list[4].weather[0].icon;
+    forecastTemp5.textContent = 'Temp: ' + forecastRes.list[4].main.temp + ' F°';
+    forecastHumidity5.textContent = 'Humidity: ' + forecastRes.list[4].main.humidity + ' %';
 }
 
-
+generateButtons()
 
 searchFormEl.addEventListener('click', handleSearchFormSubmit); //enter still works?
-// .addEventListener('click', handleSearchFormSubmit);
 
